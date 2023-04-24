0da123f93ba307898f7e9619700bc38c5c0054b4
tidso <131384740+tidso@users.noreply.github.com>
2023-04-24 16:42:47 -0700

    Add files via upload

---
+++ 1_carbon_diamond_cubic/carbon_diamond_band.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -N 2                          # number of nodes
+#SBATCH --ntasks-per-node=8           # number of MPI processes per node
+#SBATCH -t 0-02:00                    # time in d-hh:mm:ss
+
+#SBATCH --mail-type=END,FAIL          # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=tidso@asu.edu     # Mail-to address
+
+#SBATCH --output=myjob.out            # Redirect output to myjob.out
+#SBATCH --error=myjob.err             # Redirect error to myjob.err
+module purge
+module load intel-mpi/5.0
+
+srun --mpi=pmi2 /home/xpeng7/VASP5/VASP533/vasp.5.3/vasp
